0a609ee3c228c335700cc60358c8a758680e52c8
Luiz Otavio <luiz123o@hotmail.com>
2023-02-11 11:36:43 -0300

    feat: create styles in Home screen

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#131016",
+    padding: 24,
+  },
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: {
+    color: "#6B6B6B",
+    fontSize: 16,
+  },
+  input: {
+    backgroundColor: "#1F1E25",
+    height: 56,
+    borderRadius: 5,
+    color: "#FDFCFE",
+    padding: 16,
+    fontSize: 16,
+  },
+});
