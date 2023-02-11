fdce689a1ce78db8a202c1b1cce670acf9cd84de
Luiz Otavio <luiz123o@hotmail.com>
2023-02-11 11:55:46 -0300

    feat: add Touch button add participant

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,16 +1,24 @@
-import { Text, TextInput, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.eventName}>Nome do Evento!</Text>
-            <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
-            <TextInput
-                style={styles.input}
-                placeholder="nome do participante"
-                placeholderTextColor="#6B6B6B"
-            />
-        </View>
-    );
+  const handleParticipantAdd = () => {
+    console.log("Você Clicou");
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do Evento!</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022</Text>
+      <View style={styles.form}>
+        <TextInput
+          style={styles.input}
+          placeholder="nome do participante"
+          placeholderTextColor="#6B6B6B"
+        />
+        <TouchableOpacity style={styles.button} onPress={handleParticipantAdd}>
+          <Text style={styles.buttonText}>+</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
