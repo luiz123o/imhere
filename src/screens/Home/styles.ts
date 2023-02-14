d67738223a705dc7ad00ac4b067edf72a7403a15
Luiz Otavio <luiz123o@hotmail.com>
2023-02-14 08:38:09 -0300

    feat: add styles flatList

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -44,4 +44,9 @@ export const styles = StyleSheet.create({
     marginTop: 36,
     marginBottom: 42,
   },
+  listEmptyText: {
+    color: "#FDFCFE",
+    fontSize: 14,
+    textAlign: "center",
+  },
 });
